3d82223c91a6086adaef0c2bce44b4b182a971d6
rpinx <pumelotea@gmail.com>
2021-01-11 09:55:39 +0800

    test: 新增security测试

---
+++ src/security/__tests__/index.spec.ts
@@ -0,0 +1,62 @@
+import { createHappySecurity } from '../index'
+import { HAPPYKIT_STORAGE, SECURITY_TOKEN, SECURITY_USER, StorageEngine } from '../../types'
+
+test('createHappySecurity', () => {
+  const instance = createHappySecurity()
+  expect(instance.token).toBe('')
+  expect(instance.getToken()).toBe('')
+  expect(instance.user.value).toBe(null)
+  expect(instance.getUser().value).toBe(null)
+  expect(instance.options.storageEngine).toBe(StorageEngine.LOCAL_STORAGE)
+  expect(instance.getStorage()).toBe(localStorage)
+})
+
+test('storage operate', () => {
+  const instance = createHappySecurity()
+  // 准备数据
+  const keyToken = `${HAPPYKIT_STORAGE}/${SECURITY_TOKEN}`
+  const keyUser = `${HAPPYKIT_STORAGE}/${SECURITY_USER}`
+  localStorage.setItem(keyToken, 'token')
+  const user = {
+    username: 'username',
+    email: 'test@test.com',
+  }
+  localStorage.setItem(keyUser, JSON.stringify(user))
+
+  // 加载数据
+  instance.loadFromStorage()
+  expect(instance.token).toBe('token')
+  expect(instance.user.value).toStrictEqual(user)
+
+  // 清空数据
+  instance.flushStorage()
+  expect(localStorage.getItem(keyToken)).toBe(null)
+  expect(localStorage.getItem(keyUser)).toBe(null)
+
+  // 持久化数据
+  instance.saveIntoStorage()
+  expect(localStorage.getItem(keyToken)).toBe('token')
+  expect(localStorage.getItem(keyUser)).toBe('{"username":"username","email":"test@test.com"}')
+})
+
+test('signIn&signOut', () => {
+  const instance = createHappySecurity()
+  const user2 = {
+    username: 'username2',
+    email: 'test@test.com2',
+  }
+  instance.signIn('token2', user2)
+  expect(instance.getToken()).toBe('token2')
+  expect(instance.getUser().value).toStrictEqual(user2)
+
+  const keyToken = `${HAPPYKIT_STORAGE}/${SECURITY_TOKEN}`
+  const keyUser = `${HAPPYKIT_STORAGE}/${SECURITY_USER}`
+  expect(localStorage.getItem(keyToken)).toBe('token2')
+  expect(localStorage.getItem(keyUser)).toBe('{"username":"username2","email":"test@test.com2"}')
+
+  instance.signOut()
+  expect(instance.getToken()).toBe('')
+  expect(instance.getUser().value).toBe(null)
+  expect(localStorage.getItem(keyToken)).toBe(null)
+  expect(localStorage.getItem(keyUser)).toBe(null)
+})
