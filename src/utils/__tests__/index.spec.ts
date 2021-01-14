fb50ab551fcbc02c78095c99d7f960d6848295eb
pumelotea <zf_722@yeah.net>
2021-01-14 13:03:48 +0800

    test: 工具类深度拷贝测试

--- src/utils/__tests__/index.spec.ts
+++ src/utils/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { jsonReplacer } from '../index'
+import { deepClone, jsonReplacer } from '../index'
 
 test('nest map use JSON.stringify to string json', () => {
   const map = new Map<string, any>()
@@ -14,4 +14,25 @@ test('nest map use JSON.stringify to string json', () => {
     },
   }
   expect(JSON.stringify(map, jsonReplacer)).toBe(JSON.stringify(result))
+})
+
+test('deepClone', () => {
+  const source = {
+    a: 'string',
+    b: 1000.01,
+    c: true,
+    d: {
+      x: '1',
+      y: false,
+    },
+    e: [1, 2, 3],
+    f: [{ t: '1' }, { y: '2' }],
+  }
+  const target = deepClone(source)
+  expect(target).toStrictEqual(source)
+  expect(JSON.stringify(target)).toStrictEqual(JSON.stringify(source))
+
+  expect(() => {
+    deepClone('')
+  }).toThrow('error arguments shallowClone')
 })
